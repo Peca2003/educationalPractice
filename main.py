a9fd3897b2ab93a5ad35432b6792f6ff3868f229
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-04-25 15:52:30 +0300

    Update and rename test.py to main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome(
     executable_path = './chromedriver.exe'
 )
-while(45645):
+for i in range(0,2):
     driver.get("http://pyotrk6c.beget.tech/")
     element = driver.find_element(By.ID, "sec2")
     element.click()
-    element1 = driver.find_element(By.CLASS_NAME, "slider-next")
+    element = driver.find_element(By.CLASS_NAME, "slider-next")
     for i in range(0, 5):
-        element1.click()
-        time.sleep(1)
-    element2 = driver.find_element(By.CLASS_NAME, "slider-prev")
+        time.sleep(3)
+        element.click()
+    element = driver.find_element(By.CLASS_NAME, "slider-prev")
     for i in range(0, 5):
-        element2.click()
-        time.sleep(1)
+        time.sleep(3)
+        element.click()
