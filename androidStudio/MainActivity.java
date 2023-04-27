c400c262f91152b07b0cdb59dbea8d3bca33456e
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-04-27 15:08:27 +0300

    Update MainActivity.java

--- androidStudio/MainActivity.java
+++ androidStudio/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,14 +19,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setTitle("Astartes");
+
         btnWeb = (Button) findViewById(R.id.btnWeb);
         btnWeb.setOnClickListener(this);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public void onClick(View v){
         Intent intent;
+        btnWeb.setBackgroundColor(R.color.textButtonColor1);
         intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://pyotrk6c.beget.tech/"));
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
